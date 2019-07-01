1dbc85809d9443b0fae6cefad96f7ee7acbeaa64
Mathspy <mathspy257@gmail.com>
2019-07-01 02:41:48 +0200

    Added a log greeting for successfully running

--- src/main.rs
+++ src/main.rs
@@ -51,5 +51,7 @@ fn main() {
     // start listening for events by starting a single shard
     if let Err(why) = client.start() {
         println!("An error occurred while running the client: {:?}", why);
+    } else {
+        println!("I am alive!");
     }
 }
