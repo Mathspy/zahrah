e1d9e202241e3614acf9a145a07883551daa72c2
Mathspy <mathspy257@gmail.com>
2019-07-01 03:57:55 +0200

    Simplified deploy script and no longer running from root
    Special thanks to BlackHoleFox, repnop, jcotton42, Lokathor <3

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
 # Clone into said directory and go there
-cd "/root/zahrah"
+cd "~/zahrah"
 git fetch origin master
 git reset --hard origin/master
 
-# Build Zahrah in release mode and move to /opt
-cargo build --release
-cp target/release/zahrah /opt/zahrah
-
-# Go to /opt, prepare env and run Zahrah in a subshell
-cd /opt/zahrah
-(export $(grep -v '^#' .env | xargs) && ./zahrah)
+# Prepare env and build, run Zahrah in a subshell
+(export $(grep -v '^#' .env | xargs) && screen -s zahrah -d -m "cargo run --release")
